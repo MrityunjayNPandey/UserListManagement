b580e6e885b333d7fd597aa1f065a5ca8aab50ac
Mrityunjay Pandey <pandeymrityunjay796@gmail.com>
2024-05-20 19:36:52 +0530

    updated files

--- mongooseConnect.js
+++ mongooseConnect.js
@@ -1,8 +1,13 @@
+import dotenv from "dotenv";
 import mongoose from "mongoose";
+
+dotenv.config();
 
 export default function connectToDB() {
   mongoose
-    .connect("mongodb://localhost:27017/user_list_management")
+    .connect(
+      `${process.env.mongoURL}?authSource=admin&retryWrites=true&w=majority`
+    )
     .then(() => {
       console.log(`DB connected`);
     });
